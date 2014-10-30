6851d3e5757683ed0f58d2fafea455cf38a3abc2
david <gnyrfta@gmail.com>
2014-10-30 12:10:57 +0100

    third try branching today

---
+++ MusicPad/src/com/gnyrfta/musicpad/TestButton.java
@@ -0,0 +1,32 @@
+package com.gnyrfta.musicpad;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageButton;
+
+
+public class TestButton extends ImageButton {
+	private int soundToPlay;
+	public TestButton(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean onTouchEvent(final MotionEvent event) {
+		Log.v("tag", "I get touched");
+		//setText("I recive a MotionEvent");
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			//setText("I can recive Move events outside of my View");
+			Pad.mSoundPoolHelper.play(Pad.soundIdUL);
+		}
+		return super.onTouchEvent(event);
+	}
+	public int setSoundId(int soundId)
+	{
+		soundToPlay=soundId;
+		return soundToPlay;
+	}
+}
